cf36a3edd0acca9ff6458b3194083478218f9b51
Nikhil Rana <79563539+nikhilrana884@users.noreply.github.com>
2025-02-12 19:35:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import discord
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run('')
